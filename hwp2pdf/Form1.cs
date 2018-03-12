76e8002ff3e84827611d964b123bca5f3e0d0782
snowsprout <dnejrwhd@gamil.com>
2018-03-13 04:58:03 +0900

    init

---
+++ hwp2pdf/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hwp2pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            string InputFilePath = args[0];
+            string outputFilePath = args[1];
+
+            bool res = Convert(InputFilePath, outputFilePath);
+
+            if (res)
+            {
+                this.Close();
+            }
+
+        }
+
+        public bool Convert(string inputFilePath, string outputFilePath)
+
+        {
+
+            const string HNCRoot = @"HKEY_Current_User\Software\HNC\HwpCtrl\Modules";
+
+            string myProjectPath = Path.GetFullPath(".\\");
+            
+            try{
+                if (Microsoft.Win32.Registry.GetValue(HNCRoot, "FilePathChecker", "Not Exist").Equals("Not Exist")) { 
+                    Microsoft.Win32.Registry.SetValue(HNCRoot, "FilePathChecker", Environment.CurrentDirectory + "\\" + "FilePathCheckerModuleExample.dll");
+                }
+            }catch {
+                Microsoft.Win32.Registry.SetValue(HNCRoot, "FilePathChecker", Environment.CurrentDirectory + "\\" + "FilePathCheckerModuleExample.dll");
+            }
+
+            bool result = axHwpCtrl1.RegisterModule("FilePathCheckDLL", "FilePathChecker");
+            axHwpCtrl1.Open(inputFilePath);
+            axHwpCtrl1.SaveAs(outputFilePath, "PDF", null);
+            axHwpCtrl1.Clear();
+
+            return true;
+        }
+        
+
+    }
+}
---
+++ hwp2pdf/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hwp2pdf
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0 || args.Length < 1)
+                {
+                    MessageBox.Show("Incorrect number of arguments passed", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // For the sake of this example, we're just printing the arguments to the console.
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine("args[{0}] == {1}", i, args[i]);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(args));
+        }
+    }
+}
